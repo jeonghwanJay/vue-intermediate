c8cbc93109d55598231042e003beb3c4e8e8a8f3
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-01 20:21:35 +0900

    add : AlertModal edit : TodoInput

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,27 @@
     <span v-on:click="addTodo" class="addContainer">
       <i class="addBtn fa-solid fa-plus"></i>
     </span>
+
+    <Alert-Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">Warning!
+          <i class="fa-solid fa-circle-xmark" v-on:click="closeModalBtn"></i>
+        </h3>
+        <h3 slot="body"> 텍스트를 입력하세요.</h3>
+      </Alert-Modal>
   </div>
 </template>
 
 <script>
+import AlertModal from './common/AlertModal.vue'
 export default {
   data: function() {
     return {
       newTodoItem: '',
+      showModal: false,
     }
   },
   methods: {
@@ -19,9 +32,16 @@ export default {
       if(this.newTodoItem !== '') {
         this.$emit('addTodoItem', this.newTodoItem)
         this.newTodoItem = '';
+      } else {
+        this.showModal = !this.showModal
       }
-      
+    },
+    closeModalBtn: function() {
+      this.showModal = false
     }
+  },
+  components: {
+    AlertModal,
   }
 }
 </script>
